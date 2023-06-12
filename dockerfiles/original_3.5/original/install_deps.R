c95bb7d2863fd5568fe1aa9f04af436a7df2dabc
Bruno Rodrigues <bruno@brodrigues.co>
2023-06-12 16:21:38 +0200

    added results of script running on R 4.2 with library as of may 2023 (but zelig and zeligchoice installed manually, as matching

--- dockerfiles/original_3.5/original/install_deps.R
+++ dockerfiles/original_3.5/original/install_deps.R
@@ -1,15 +1,3 @@
-# Old versions of Zelig and ZeligChoice
-# Actually the below is likely not needed, groundhog should be able to get the required versions
-#install.packages('http://cran.r-project.org/src/contrib/Archive/Zelig/Zelig_5.1.6.tar.gz',
-#                 repos=NULL,
-#                 type='source',
-#                 lib = groundhog_lib)
-#
-#install.packages('http://cran.r-project.org/src/contrib/Archive/ZeligChoice/ZeligChoice_0.9-6.tar.gz',
-#                 repos=NULL,
-#                 type='source',
-#                 lib = groundhog_lib)
-
 # the paper was published in may 2020, so set the date accordingly below
 # Also, the package library used was likely older than may 2020: this is because
 # in may 2020 {foreign} was on version 0.8-79, but that version already required
@@ -24,29 +12,45 @@
 # Let's try install to install the right version from github
 #install.packages("remotes")
 #
-#remotes::install_github("JasjeetSekhon/Matching",
-#                        ref = "529d1ad", # commit with version 4.9-6
-#                        )
+# The below is needed if running on R 4.2
+# Comment otherwise
+install.packages("remotes")
+remotes::install_github("JasjeetSekhon/Matching",
+                        ref = "529d1ad", # commit with version 4.9-6
+                        )
 # but then had an issue with the library not being writeable
 # The solution I've settled on is using Posit's CRAN snapshots. First, I tried December 
 # 2019, but the version of MCMCpack that was available at the time required R 3.6.
 # So now, I've settled to May 2019, and now the dependencies get installed.
 
-# I don't think the below is needed
-#install.packages(
-#  c("sandwich",
-#    "VGAM",
-#    "jsonlite",
-#    "AER",
-#    "plyr",
-#    "dplyr",
-#    "quantreg",
-#    "geepack",
-#    "MCMCpack",
-#    "maxLik",
-#    "Amelia",
-#    "MatchIt",
-#    "survey"))
+# The below is need if installing Zelig and ZeligChoice from source (if running on R 4.2)
+# Comment otherwise
+install.packages(
+  c("sandwich",
+    "VGAM",
+    "jsonlite",
+    "AER",
+    "plyr",
+    "dplyr",
+    "quantreg",
+    "geepack",
+    "MCMCpack",
+    "maxLik",
+    "Amelia",
+    "MatchIt",
+    "survey"))
+
+# Old versions of Zelig and ZeligChoice
+# Only use if running the script on R version 4.2
+install.packages('http://cran.r-project.org/src/contrib/Archive/Zelig/Zelig_5.1.6.tar.gz',
+                 repos=NULL,
+                 type='source')
+
+install.packages('http://cran.r-project.org/src/contrib/Archive/ZeligChoice/ZeligChoice_0.9-6.tar.gz',
+                 repos=NULL,
+                 type='source')
+
+# The list below are the required packages for the project
 
 install.packages(
   c("MASS",
@@ -56,8 +60,8 @@ install.packages(
     "purrr",
     "broom",
     "foreign",
-    "Zelig",
-    "ZeligChoice",
+#    "Zelig", #Comment if running on R 4.2
+#    "ZeligChoice", #Comment if running on R 4.2
     "nnet",
     "car",
     "texreg",
