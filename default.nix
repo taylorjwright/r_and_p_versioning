9be51419ce7653bed46439f9f69422846d84e27a
Bruno Rodrigues <brodriguesco@protonmail.com>
2023-11-02 17:18:53 +0100

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/976fa3369d722e76f37c77493d99829540d43845.tar.gz") {};
  rpkgs = builtins.attrValues {
-  inherit (pkgs.rPackages) knitr dplyr tidyr stringr purrr ggplot2 flextable quarto;
+  inherit (pkgs.rPackages) knitr dplyr tidyr stringr purrr ggplot2 flextable quarto targets tarchetypes;
 };
   tex = (pkgs.texlive.combine {
   inherit (pkgs.texlive) scheme-small amsmath booktabs setspace lineno cochineal tex-gyre framed multirow wrapfig tcolorbox environ tikzfill pdfcol;
